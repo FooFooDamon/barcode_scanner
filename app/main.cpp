460abc6d54794765df638b2522223de202954c14
FooFooDamon <udc577@126.com>
2024-05-04 21:42:51 +0800

    app/main.cpp: Implement a demo of detecting from an image file.

--- app/main.cpp
+++ app/main.cpp
@@ -18,11 +18,67 @@
 
 #include <stdio.h>
 
+#include <string>
+#include <algorithm>
+#include <iostream>
+
+#include <opencv2/core/types.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <ZXing/BarcodeFormat.h>
+#include <ZXing/DecodeHints.h>
+#include <ZXing/ReadBarcode.h>
+#include <ZXing/Result.h>
+#include <ZXing/DecodeStatus.h>
+#include <QtCore/QString>
+
 int main(int argc, char **argv)
 {
-    printf("TODO ...\n");
+    if (argc < 2)
+    {
+        std::cerr << "Usage: " << argv[0] << " /path/to/barcode/image" << std::endl;
+        return EXIT_FAILURE;
+    }
 
-    return 0;
+    cv::Mat image = cv::imread(argv[1], cv::IMREAD_COLOR);
+    auto img_view = ZXing::ImageView(image.data, image.cols, image.rows, ZXing::ImageFormat::BGR);
+    ZXing::DecodeHints hints;
+    const auto &result = ZXing::ReadBarcode(img_view, hints.setFormats(ZXing::BarcodeFormat::Any));
+
+    if (ZXing::DecodeStatus::NoError != result.status())
+    {
+        std::cerr << "Failed to detect: " << ZXing::ToString(result.status()) << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    const auto &pos = result.position();
+    const auto &top_left = pos.topLeft();
+    const auto &bottom_right = pos.bottomRight();
+    auto center = ZXing::Position::Point(std::min(top_left.x, bottom_right.x) + abs(bottom_right.x - top_left.x) / 2,
+        std::min(top_left.y, bottom_right.y) + abs(bottom_right.y - top_left.y) / 2);
+    const cv::Scalar color(0, 0, 255);
+    const int thickness = 2;
+
+#if 0
+    cv::rectangle(image, cv::Point(top_left.x, top_left.y), cv::Point(bottom_right.x, bottom_right.y),
+        color, thickness, cv::LineTypes::LINE_AA);
+#endif
+    for (const auto &p : { top_left, pos.topRight(), pos.bottomLeft(), bottom_right, center })
+    {
+        cv::drawMarker(image, cv::Point(p.x, p.y), color, cv::MarkerTypes::MARKER_DIAMOND,
+            /* markerSize = */20, thickness);
+    }
+    cv::imshow("Barcode Test", image);
+    cv::waitKey(0);
+    std::cout << "Type: " << ZXing::ToString(result.format()) << std::endl
+        << "Text: " << QString::fromStdWString(result.text()).toStdString() << std::endl
+        << "Orientation: " << result.orientation() << std::endl
+        << "Error Correction Level: " << QString::fromStdWString(result.ecLevel()).toStdString() << std::endl
+        << "Bits: " << result.numBits() << std::endl;
+
+    return EXIT_SUCCESS;
 }
 
 /*
