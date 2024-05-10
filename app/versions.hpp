12db8663231e7de682f9734cb483f2802b43de4a
FooFooDamon <udc577@126.com>
2024-05-10 20:14:52 +0800

    app: + versions.hpp: Version numbers.

---
+++ app/versions.hpp
@@ -0,0 +1,48 @@
+/*
+ * Version numbers.
+ *
+ * Copyright (c) 2024 Man Hung-Coeng <udc577@126.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+*/
+
+#ifndef __VERSIONS_HPP__
+#define __VERSIONS_HPP__
+
+#ifndef MAJOR_VER
+#define MAJOR_VER                       0
+#endif
+
+#ifndef MINOR_VER
+#define MINOR_VER                       1
+#endif
+
+#ifndef PATCH_VER
+#define PATCH_VER                       0
+#endif
+
+#ifndef __VER__
+#define __VER__                         "<none>"
+#endif
+
+#endif /* #ifndef __VERSIONS_HPP__ */
+
+/*
+ * ================
+ *   CHANGE LOG
+ * ================
+ *
+ * >>> 2024-05-10, Man Hung-Coeng <udc577@126.com>:
+ *  01. Create.
+ */
+
