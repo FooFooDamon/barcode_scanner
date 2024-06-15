ce7bb7dfc61736ce86e2f779bc5e9d09ca668888
FooFooDamon <udc577@126.com>
2024-06-15 20:26:56 +0800

    3rdpary/lazy_coding.git.chkout.mk: 3b93336d4e3e4c2f46fd795f2acf2fd437c08c9c/c_and_cpp/native/signal_handling.c

--- 3rdpary/lazy_coding.git.chkout.mk
+++ 3rdpary/lazy_coding.git.chkout.mk
@@ -10,7 +10,7 @@ export CHKOUT_URL := https://github.com/FooFooDamon/lazy_coding_skills
 export CHKOUT_TAIL_PARAMS :=
 export CHKOUT_PARTIAL_ITEMS := main/makefile/__ver__.mk \
     ac3698898d984427981cef75c4c5cd0aa09b3c16/makefile/c_and_cpp.mk \
-    38cf125e08241b9330812f6b040a072b6116adf6/c_and_cpp/native/signal_handling.c \
+    3b93336d4e3e4c2f46fd795f2acf2fd437c08c9c/c_and_cpp/native/signal_handling.c \
     38cf125e08241b9330812f6b040a072b6116adf6/c_and_cpp/native/signal_handling.h \
     # Add more items ahead of this line if needed. \
     # Beware that each line should begin with 4 spaces and end with a backslash.
